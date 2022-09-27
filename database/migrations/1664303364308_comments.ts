e3d40b80797f98eb8fd77142ea938df7fb575e66
Felipe Ribeiro <felipedejesusifgoiano@gmail.com>
2022-09-27 15:49:22 -0300

    new file:   app/Controllers/Http/CommentsController.ts 	modified:   app/Controllers/Http/MomentsController.ts 	renamed:    app/Models/Coment.ts -> app/Models/Comment.ts 	modified:   app/Models/Moment.ts 	renamed:    database/migrations/1664244345455_coments.ts -> database/migrations/1664303364308_comments.ts 	modified:   start/routes.ts

--- database/migrations/1664303364308_comments.ts
+++ database/migrations/1664303364308_comments.ts
@@ -1,11 +1,15 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'coments'
+  protected tableName = 'comments'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('username')
+      table.string('text')
+
+      table.integer('moment_id').unsigned().references('moments.id').onDelete('CASCADE')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
